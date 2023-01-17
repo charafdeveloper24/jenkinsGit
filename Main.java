7fae89045c81533a454d95ae65dc501d42226998
charafdeveloper1 <charafdeveloper1@gmail.com>
2023-01-17 13:19:02 +0100

    added the 2nd line

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Line added: 01 ");        
+        System.out.println("Line added: 01");    
+        System.out.println("Line added: 02");      
     }
 }
