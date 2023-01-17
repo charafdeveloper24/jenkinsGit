daf1762b16419cda311279f4e747a355de31ae0b
charafdeveloper1 <charafdeveloper1@gmail.com>
2023-01-17 14:15:16 +0100

    add 3rd line

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Line added: 01");    
-        System.out.println("Line added: 02");      
+        System.out.println("Line added: 02");     
+        System.out.println("Line added: 03");   
     }
 }
