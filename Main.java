05898d41fc6c040c270328a9eef0f36ce50d7ada
charafdeveloper1 <charafdeveloper1@gmail.com>
2023-01-18 14:50:37 +0100

    hello 01

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Line added: 01 ");    
-        System.out.println("Line added: 02 "); 
-        System.out.println("Line added: 03 "); 
+        System.out.println("Hello: 01 ");         
     }
 }
 
